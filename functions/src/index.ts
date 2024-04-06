7af56233dfb06e94f20409f21fc8267db573dd30
onkr0d <90716666+onkr0d@users.noreply.github.com>
2024-04-06 19:42:13 -0400

    add fuctions, remove caching (it was slow)

---
+++ functions/src/index.ts
@@ -0,0 +1,31 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import {onCall, onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+exports.protectedHelloWorld = onCall(
+    {
+        enforceAppCheck: true, // Reject requests with missing or invalid App Check tokens.
+    },
+    (request) => {
+        // request.app contains data from App Check, including the app ID.
+        // Your function logic follows.
+        console.log("received request from app", request.app?.appId);
+    }
+);
+
+
+export const helloWorld = onRequest((request, response) => {
+    logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+});

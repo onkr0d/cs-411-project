b9009f649777019b0ca9578a48225e811e93bab7
Steven Yao <stevenyao2016@gmail.com>
2024-04-20 18:48:28 -0400

    debugging function error: invalid args

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,14 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 import * as functions from "firebase-functions";
-import {onCall, onRequest} from "firebase-functions/v2/https";
+import {onCall} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
 import {Timestamp} from "firebase-admin/firestore";
 
 // https://github.com/firebase/firebase-admin-node/discussions/1959#discussioncomment-3985176
 import {
+  //AccountsGetRequest,
     Configuration,
     CountryCode,
     InstitutionsGetByIdRequest,
@@ -23,6 +24,7 @@ import {
     PlaidApi,
     PlaidEnvironments,
     Products,
+    //TransactionsGetRequest,
 } from "plaid";
 
 const configuration = new Configuration({
@@ -35,7 +37,7 @@ const configuration = new Configuration({
     },
 });
 const plaidClient = new PlaidApi(configuration);
-
+//const accessToken = "";
 admin.initializeApp();
 
 // nifty little function that creates a user document for when someone signs up
@@ -107,17 +109,72 @@ exports.createNewLinkToken = onCall(
         return {error: "Error in creating link token"};
     },
 );
+/*
+exports.accountBalGet = onCall(
+  {
+    enforceAppCheck: true,
+  },
+  async(request) => {
+    const procRequest: AccountsGetRequest = {
+      access_token: accessToken,
+    };
+    try {
+      const response = await plaidClient.accountsBalanceGet(procRequest);
+      const accounts = response.data.accounts;
+      logger.log(accounts);
+      return {accounts: accounts};
+    } catch (error) {
+      return {error: error};
+    }
+  }
 
+)
+exports.transactionsGet = onCall (
+
+  {
+    enforceAppCheck: true,
+  },
+  async(request) => {
+    const procRequest: TransactionsGetRequest = {
+      access_token: accessToken,
+      start_date: '2018-01-01',
+      end_date: '2020-02-01'
+    };
+    try {
+      const response = await plaidClient.transactionsGet(procRequest);
+      let transactions = response.data.transactions;
+      //const total_transactions = response.data.total_transactions;
+      return {transactions: transactions};
+    } catch (error) {
+      return {error: error};
+    }
+  }
+)
+*/
+exports.getCatgeories = onCall(
+  {
+    enforceAppCheck: true,
+  },
+  async (request) => {
+    try {
+      const response = await plaidClient.categoriesGet({});
+      const categories = response.data.categories;
+      return categories;
+    } catch (error) {
+      return error;
+    }
+  }
+)
 
 // problem with these functions is that they would timeout rather than actually finish
 // they will log everything in console, but still would timeout instead of finishing execution
 // https://plaid.com/docs/api/institutions/#institutionsget
-exports.insitutionGet = onRequest(
+exports.insitutionGet = onCall(
     {
         // Reject requests with missing or invalid App Check tokens.
         enforceAppCheck: true,
     },
-    async (request, response) => {
+    async (request) => {
         const procRequest: InstitutionsGetRequest = {
             count: 1, // request.query.count
             offset: 0, // request.query.offset
@@ -128,8 +185,9 @@ exports.insitutionGet = onRequest(
             const institutions = response.data.institutions;
             // need to figure out how to send proper response
             logger.log(institutions);
+            return institutions;
         } catch (error) {
-            response.send("Error in getting institutions");
+            return error;
         }
     },
 );
@@ -137,7 +195,7 @@ exports.insitutionGet = onRequest(
 exports.institutionGetById = onCall(
     {
         // Reject requests with missing or invalid App Check tokens.
-        enforceAppCheck: true,
+        enforceAppCheck: false,
     },
     async (request) => {
         const procRequest: InstitutionsGetByIdRequest = {
@@ -151,17 +209,17 @@ exports.institutionGetById = onCall(
             logger.info(institution);
             return {institution: institution};
         } catch (error) {
-            return {error: "Error in getting institution"};
+            return {error: error};
         }
     },
 );
 // https://plaid.com/docs/api/institutions/#institutionssearch
-exports.institutionsSearch = onRequest(
+exports.institutionsSearch = onCall(
     {
         // Reject requests with missing or invalid App Check tokens.
         enforceAppCheck: true,
     },
-    async (request, response) => {
+    async (request) => {
         const procRequest: InstitutionsSearchRequest = {
             query: "ins_118923",
             products: [Products.Transactions],
@@ -172,8 +230,9 @@ exports.institutionsSearch = onRequest(
             const institution = response.data.institutions;
             // need to figure out how to send proper response
             logger.info(institution);
+            return institution;
         } catch (error) {
-            response.send("Error in getting institution");
+          return error;
         }
     },
 );
